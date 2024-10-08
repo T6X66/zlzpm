d4d72e4bfbf9a9926211ca3fb7cc6e040082bde9
T6X66 <bpbslo7@gmail.com>
2024-10-08 16:00:58 +0300

    Delete mob directory

--- mob/casery.py
+++
@@ -1,16 +0,0 @@
-##مطور السورس
-caes = ["S_V_VV"]
-  𝒎𝒐𝒅 ❤️y = "S_V_VV"
-##ايدي مطور السورس
-  𝒎𝒐𝒅 ❤️id = 7321431476
-##قناه السورس
-source = "https://t.me/vip0139" 
-ch = "vip0139" 
-##جروب السورس
-group = "https://t.me/vip0139" 
-##لوجو السورس
-photosource = "https://t.me/v777777bvgjvffd/2"
-##توكن الصانع
-bot_token="7957265251:AAFu4W0fU0O-CUEjEIS4l9cQ4iyXTuHghmQ"
-##جلسه الاشتراك
-bot_token2="BAF_HLYAuD751UAd-vdXu1fMu-QBy26HP-siUaiSCyAr-gK9Q6zftJnGf9oBcSLZy2sFvRrQDBSrjPbZo6fhNs9KA5IruHtFavujvZbbUPX-4m-USvaHl2npJlz6h62odBbNkmae07Ur-13pzJOlVevkacwSvwNZuRyT1h1T1heiNnmBesFnDBf-erl37qClglTKx0uduc15buu1jw8R8ruEcLQZGgQjG5IxvuRxOFe3vzEQCnm-DlhkKbaLWiz9cN1P6Guda09LOKbs7njrL5iMXRFYeHnQKqSoQUG26-eMAJrWQtKkG_ZPdD7MKUS5OH17YOMahkLFon-5VEgqmXkYkwQqWwAAAAHEHlfjAA"
